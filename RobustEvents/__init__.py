6d6e2a610a6c73d503d0bb2a0ebbbd75e22e1035
DevelopmentCats <chris@dualriver.com>
2024-07-29 10:55:16 -0500

    Update __init__.py

--- RobustEvents/__init__.py
+++ RobustEvents/__init__.py
@@ -1,3 +1,6 @@
+from redbot.core.bot import Red
+from .RobustEvents import RobustEventsCog  # Import the cog class from RobustEvents.py
+
 async def setup(bot: Red):
     cog = RobustEventsCog(bot)
     await bot.add_cog(cog)
