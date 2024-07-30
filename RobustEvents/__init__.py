992ae4048b62d2f8d32c29e3356947873349806e
DevelopmentCats <chris@dualriver.com>
2024-07-29 20:48:44 -0500

    Update __init__.py

--- RobustEvents/__init__.py
+++ RobustEvents/__init__.py
@@ -4,9 +4,4 @@ from .RobustEvents import RobustEventsCog  # Import the cog class from RobustEve
 __red_end_user_data_statement__ = "This cog does not persistently store data about users."
 
 async def setup(bot: Red):
-    try:
-        cog = RobustEventsCog(bot)
-        await bot.add_cog(cog)
-        bot.log.info("RobustEvents has been loaded and is ready.")
-    except Exception as e:
-        bot.log.exception(f"Failed to load RobustEvents: {e}")
+    await bot.add_cog(RobustEventsCog(bot))
