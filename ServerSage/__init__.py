3a67c66f21869fbd0eaa122e0b6cb907888b4a08
DevelopmentCats <chris@dualriver.com>
2025-01-02 21:37:08 -0600

    Update __init__.py

--- ServerSage/__init__.py
+++ ServerSage/__init__.py
@@ -6,6 +6,8 @@ A Red-DiscordBot cog that POUNCES on server organization problems!
 """
 
 from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+from pathlib import Path
 from .serversage import ServerSage
 
 __author__ = 'DevelopmentCats'
@@ -13,4 +15,10 @@ __version__ = '1.0.0'
 
 async def setup(bot: Red) -> None:
     """This function is called by Red when loading the cog"""
-    await bot.add_cog(ServerSage(bot))
+    cog = ServerSage(bot)
+    # This is the key part that tells Red about our requirements
+    if hasattr(cog, "requirements"):
+        requirements_file = Path(__file__).parent / "requirements.txt"
+        if requirements_file.exists():
+            cog.requirements = requirements_file.read_text().splitlines()
+    await bot.add_cog(cog)
